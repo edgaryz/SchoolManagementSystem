acaf97be75271cc53fb9db90512b43a784bcf4d9
Kirill Bravin <kshaadoo1991@gmail.com>
2024-07-25 11:16:41 +0300

    Added Student Grade management class

--- SchoolManagementSystem/Models/Student Grade Management.cs
+++ SchoolManagementSystem/Models/Student Grade Management.cs
@@ -8,6 +8,15 @@ namespace SchoolManagementSystem.Models
 {
     public class Student_Grade_Management
     {
+        public void AddGrade(Student student, Teacher teacher, Grade grade)
+        {
+            student.AddGrade(grade);
+            Console.WriteLine($"{teacher.FirstName} {teacher.LastName} has graded {student.Name} {student.SurName} with grade {grade}");
+        }
 
+        public void GetTeacherStudentGrades(Teacher teacher)
+        {
+
+        }
     }
 }
