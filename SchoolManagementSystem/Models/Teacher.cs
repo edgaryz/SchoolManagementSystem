e1ccc49ffda5eb5722086dc3658a4b2ae8d30a28
Kirill Bravin <kshaadoo1991@gmail.com>
2024-07-25 10:31:08 +0300

    Add Teacher class and Teacher Management

---
+++ SchoolManagementSystem/Models/Teacher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Models
+{
+    public class Teacher
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Subject { get; set; }
+
+        public Teacher (string firstName, string lastName, string subject)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Subject = subject;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Teacher - {FirstName} {LastName} teaches {Subject}");
+        }
+    }
+}
---
+++ SchoolManagementSystem/Models/TeacherManagement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementSystem.Models
+{
+    public class TeacherManagement
+    {
+        public List<Teacher> Teachers  { get; set; }
+
+        public void AddTeacher(Teacher teacher)
+        {
+            Teachers = new List<Teacher> { teacher };
+        }
+
+
+    }
+}
