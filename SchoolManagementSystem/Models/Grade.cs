67c33dd181e15642773a52e3f87a40431e99060d
Edgar Sokol <edgar.sokol@gmail.com>
2024-07-25 10:33:58 +0300

    Grade class

--- SchoolManagementSystem/Models/Grade.cs
+++ SchoolManagementSystem/Models/Grade.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SchoolManagementSystem.Models
+﻿namespace SchoolManagementSystem.Models
 {
     public class Grade
     {
+        public String Subject { get; set; }
+        public DateOnly GradeDate { get; set; }
+        public int GradeValue { get; set; }
+
+        public Grade() { }
+
+        public Grade(string subject, DateOnly gradeDate, int gradeValue)
+        {
+            Subject = subject;
+            GradeDate = gradeDate;
+            GradeValue = gradeValue;
+        }
     }
 }
