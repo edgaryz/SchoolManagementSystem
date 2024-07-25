bb36c71202d0a158ed354b9e0bd647dbb9d13362
Edgar Sokol <edgar.sokol@gmail.com>
2024-07-25 10:49:20 +0300

    Management Service Class

---
+++ SchoolManagementSystem/Service/StudentManagementService.cs
@@ -0,0 +1,34 @@
+﻿using SchoolManagementSystem.Models;
+
+namespace SchoolManagementSystem.Service
+{
+    public class StudentManagementService
+    {
+        public List<Student> Students { get; set; }
+        public StudentManagementService()
+        {
+            Students = new List<Student>();
+        }
+
+        public void AddStudent(Student student)
+        {
+            Students.Add(student);
+        }
+
+        public void GetAllStudents()
+        {
+            foreach (Student student in Students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+
+        public void GetStudentGrades(Student student)
+        {
+            foreach(var studentGrade in student.Grades)
+            {
+                Console.WriteLine(studentGrade);
+            }
+        }
+    }
+}
